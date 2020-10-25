45cf86943b98dcdc071454f31511f14d71dba73e
Anderson Silva <andeerson0017@gmail.com>
2020-10-25 13:28:35 -0300

    :sparkles: feat: create component Loading

---
+++ src/components/Loading/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Container = styled.div`
+  position:fixed;
+  left: 0;
+  top: 0;
+  bottom: 0;
+  right: 0;
+  z-index: 99;
+  background: #000;
+  display:flex;
+  align-items: center;
+  justify-content:center;
+`;
