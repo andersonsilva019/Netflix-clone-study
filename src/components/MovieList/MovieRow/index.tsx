6e3b8c19ed3cce3ed83a3e8f77fd7db24e812a87
Anderson Silva <andeerson0017@gmail.com>
2020-10-25 13:29:14 -0300

    :sparkles: feat: add scroll MovieList

--- src/components/MovieList/MovieRow/index.tsx
+++ src/components/MovieList/MovieRow/index.tsx
@@ -1,4 +1,6 @@
 import * as S from './styles';
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from 'react-icons/md';
+import { useState } from 'react';
 
 
 export interface IMovieData {
@@ -14,11 +16,41 @@ interface IMovieProps {
 }
 
 export default function Movie({ title, items }: IMovieProps) {
+
+  const [scrollX, setScrollX] = useState(-1200)
+
+  const handleLeftArrow = () => {
+    let x = scrollX + Math.round(window.innerWidth / 2)
+    if (x > 0) {
+      x = 0
+    }
+
+    setScrollX(x);
+  }
+  const handleRightArrow = () => {
+    let x = scrollX - Math.round(window.innerWidth / 2)
+    const widthList = items.results.length * 200
+    if ((window.innerWidth - widthList) > x) {
+      x = (window.innerWidth - widthList) - 60
+    }
+    setScrollX(x)
+  }
+
   return (
     <S.Container>
       <h2>{title}</h2>
+
+      <S.KeyboardArrowLeft onClick={handleLeftArrow}>
+        <MdKeyboardArrowLeft size={50} />
+      </S.KeyboardArrowLeft>
+
+      <S.KeyboardArrowRight onClick={handleRightArrow}>
+        <MdKeyboardArrowRight size={50} />
+      </S.KeyboardArrowRight>
+
+
       <S.MovieListArea>
-        <S.MovieList>
+        <S.MovieList style={{ marginLeft: scrollX, width: items.results.length * 300 }}>
           {items.results.length > 0 && items.results.map((item, key) => (
             <S.Item key={key}>
               <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt={item.name} />
--- src/components/MovieList/MovieRow/styles.ts
+++ src/components/MovieList/MovieRow/styles.ts
@@ -1,10 +1,44 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
+
+
+const KeyboardArrowStyles = css`
+  position: absolute;
+  width: 40px;
+  height: 300px;
+  background: rgba(0,0,0,0.6);
+  z-index: 99;
+  display: flex;
+  align-items:center;
+  justify-content:center;
+  overflow: hidden;
+  cursor: pointer;
+  opacity: 0;
+  transition: all ease 0.5s;
+`
+
+export const KeyboardArrowLeft = styled.div`
+  ${KeyboardArrowStyles};
+  left: 0;
+`
+export const KeyboardArrowRight = styled.div`
+  ${KeyboardArrowStyles};
+  right: 0;
+`
 
 export const Container = styled.div`
   margin-bottom: 30px;
 
   > h2 {
     margin: 0 0 0 30px;
+  }
+
+  :hover {
+    ${KeyboardArrowLeft} {
+      opacity: 1;
+    }
+    ${KeyboardArrowRight} {
+      opacity: 1;
+    }
   }
 `;
 
@@ -14,11 +48,11 @@ export const MovieListArea = styled.div`
 `;
 
 export const MovieList = styled.div`
-  width: 9999px;
+  transition: all ease 0.5s;
 `;
 
 export const Item = styled.div`
-  width: 150px;
+  width: 200px;
   display: inline-block;
   cursor: pointer;
   > img {
