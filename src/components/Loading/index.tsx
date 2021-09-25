c47e351a810d4a42e13f358d3a6c637b87a7a7d3
Anderson Silva <andersonnsilva@alu.ufc.br>
2021-09-25 09:45:41 -0300

    chore: deletando arquivos nao utilizados

--- src/components/Loading/index.tsx
+++
@@ -1,8 +0,0 @@
-import * as S from './styles'
-export default function Loading() {
-  return (
-    <S.Container>
-      <img src="https://www.filmelier.com/pt/br/news/wp-content/uploads/2020/03/Netflix_LoadTime-scaled.gif" alt="Carregando" />
-    </S.Container>
-  )
-}
